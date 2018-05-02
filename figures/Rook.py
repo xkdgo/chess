36a4c4f8d7242a8c3834e3147a34da30c5adaa40
xkdgo <xkdgo@rambler.ru>
2018-05-02 19:16:34 +0500

    add Rook file

---
+++ figures/Rook.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from exceptions import InvalidMove
+from .Figure import Figure
+
+
+class Rook(Figure):
+
+    def assert_legal_move(self, new_pos):
+        x0, y0 = self.pos
+        x1, y1 = new_pos
+        if x0 != x1 and y0 != y1:
+            raise InvalidMove
+
+    @property
+    def symbol(self):
+         return 'R' + self.color
